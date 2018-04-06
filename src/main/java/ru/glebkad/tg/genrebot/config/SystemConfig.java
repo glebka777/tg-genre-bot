fb92aeca89caf2029ea75f9bbd06e2247e1c99a3
glebka777 <glebdo777@gmail.com>
2018-04-07 02:16:43 +0300

    update system properties setups for configs

--- src/main/java/ru/glebkad/tg/genrebot/config/SystemConfig.java
+++ src/main/java/ru/glebkad/tg/genrebot/config/SystemConfig.java
@@ -6,8 +6,8 @@ public class SystemConfig {
     public static final String LAST_FM_API_KEY;
 
     static {
-        TG_BOT_API_KEY = System.getProperty("tg.bot.api.key");
-        LAST_FM_API_KEY = System.getProperty("last.fm.api.key");
+        TG_BOT_API_KEY = System.getProperty("tg.bot.api.key") == null ? System.getenv("tg.bot.api.key") : System.getProperty("tg.bot.api.key");
+        LAST_FM_API_KEY = System.getProperty("last.fm.api.key") == null ? System.getenv("last.fm.api.key") : System.getProperty("last.fm.api.key");
     }
 
 }
